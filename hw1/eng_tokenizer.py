644f09daae60bdd8d81de38c6e550fe3a3332379
HaoboGu <haobogu@outlook.com>
2017-10-02 16:52:07 -0700

    Framework of tokenier completed

--- hw1/eng_tokenizer.py
+++ hw1/eng_tokenizer.py
@@ -1,31 +1,65 @@
-#!/usr/bin/python
+# !/usr/bin/python
 
-"""%prog [--help] [N]
+"""
 
-This script prints the first N lines from STDIN.  If N is not specified, STDIN
-is printed until exhausted."""
+This script is a tokenizer used to split texts to words by punctuations/whitespaces. Some phrases won't be splitted.
+
+"""
 
 import sys
+import re
 
+def tokenize(string, abbrev_file):
+    regex = ' '
+    punctuations = ['.', ',', '-', '%', '#', '!', '=', '$', '&']
+    pattern = re.compile(regex) # compile a regex to a pattern
+    match = pattern.match(line)
+    if match:
+        # if there is somerhing in line matches pattern
+        print(match.groups())
+    return 1
 
-def tokenizer(abbrev_file):
-        """Print the first n lines from STDIN
+def create_abbrev_list(abbrev_file):
+    f = open(abbrev_file)
+    line = f.readline()
+    abbrev_list = []
+    while line:
+        line = line.rstrip('\n')
+        abbrev_list.append(line)
+        line = f.readline()
+    f.close()
+    return abbrev_list
+
+def process(abbrev_file):
+        """Process the input_file, generate output_file
         """
-        line = sys.stdin.readline()
+        use_file = 1
+        if use_file == 1:
+            input_file = open('input_file','r')
+            line = input_file.readline()
+        else:
+            line = sys.stdin.readline()
+        abbrev_list = create_abbrev_list(abbrev_file)
         while line:
-            # TODO: tokenize the line
             # line = tokenize(line)
             line = line.rstrip('\n')
-            print(line)
-            line = sys.stdin.readline()
+            # output_line = tokenize(line, abbrev_list)
+            if use_file == 1:
+                line = input_file.readline()
+            else:
+                line = sys.stdin.readline()
+        if use_file == 1:
+            input_file.close()
 
 if __name__ == "__main__":
         from optparse import OptionParser
         
         parser = OptionParser(__doc__)
         options, args = parser.parse_args()
+        args = ['abbrev-list']
         if len(args) == 0:
             print("No abbrev-list file!")
         else:
-            tokenizer(args[0])    
+            process(args[0])    
+            
         # n_lines_from_stdin(n)
